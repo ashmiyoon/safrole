788386c5b1017ec479ba494953a0c2adc3007422
ashmiyoon <ashmiyoon@gmail.com>
2023-08-24 14:50:49 -0400

    Create cik.ex

---
+++ lib/cik.ex
@@ -0,0 +1,28 @@
+defmodule Safrole.Cik do
+  @spec download_ticker_txt() :: {atom(), String.t()}
+  def download_ticker_txt() do
+    ticker_txt_url = "https://www.sec.gov/include/ticker.txt"
+    with {:ok, response} <- HTTPoison.get(ticker_txt_url),
+         200 <- response.status_code,
+         txt <- response.body
+    do
+      {:ok, txt}
+    else
+      err -> {:err, inspect(err)}
+    end
+  end
+
+  def txt_to_stock_infos(raw_txt) do
+    lines = String.split(raw_txt, "\n")
+    lines
+    |> Enum.map(fn line ->
+      with [raw_ticker, raw_cik] <- String.split(line, "\t"),
+           stock_info <- %{ticker: String.upcase(raw_ticker), cik: raw_cik}
+      do
+        stock_info
+      else
+        err -> err
+      end
+    end)
+  end
+end
