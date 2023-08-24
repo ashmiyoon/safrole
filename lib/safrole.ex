446d61d4be19739f537118865095dac94cf0bf96
ashmiyoon <ashmiyoon@gmail.com>
2023-08-24 13:32:08 -0400

    Mix project creation

---
+++ lib/safrole.ex
@@ -0,0 +1,18 @@
+defmodule Safrole do
+  @moduledoc """
+  Documentation for `Safrole`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Safrole.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/safrole_test.exs
@@ -0,0 +1,8 @@
+defmodule SafroleTest do
+  use ExUnit.Case
+  doctest Safrole
+
+  test "greets the world" do
+    assert Safrole.hello() == :world
+  end
+end
