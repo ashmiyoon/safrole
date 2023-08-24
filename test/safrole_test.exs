ba3eb1ce3f924acb50d6f895827b59b2316f77bc
ashmiyoon <ashmiyoon@gmail.com>
2023-08-24 14:56:23 -0400

    Added first test

--- test/safrole_test.exs
+++ test/safrole_test.exs
@@ -5,4 +5,10 @@ defmodule SafroleTest do
   test "greets the world" do
     assert Safrole.hello() == :world
   end
+
+  test "creates stock info map" do
+    {:ok, ticker_txt} = Safrole.Cik.download_ticker_txt()
+    stock_info_map = Safrole.Cik.txt_to_stock_infos(ticker_txt)
+    IO.inspect stock_info_map
+  end
 end
