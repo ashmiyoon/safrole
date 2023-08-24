bc59b49942c1462fe4edbf6b19ad344a7cb21352
ashmiyoon <ashmiyoon@gmail.com>
2023-08-24 14:15:20 -0400

    Added Floki Dependency

--- mix.exs
+++ mix.exs
@@ -23,6 +23,7 @@ defmodule Safrole.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:floki, "~> 0.34.0"}
     ]
   end
 end
